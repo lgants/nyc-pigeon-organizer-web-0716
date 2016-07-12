c614f06a39e042e6fb5175b1e19f285e0d4a355b
lgants <lgants@gmail.com>
2016-07-12 17:30:16 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |supra_attribute, sub_attribute|
+    sub_attribute.each do |sub_attribute, names|
+      names.each do |name|
+        if !pigeon_list[name] 
+          pigeon_list[name] = {}
+        end 
+        if !pigeon_list[name][supra_attribute]
+          pigeon_list[name][supra_attribute] = []
+        end 
+        pigeon_list[name][supra_attribute].push(sub_attribute.to_s)
+      end 
+    end 
+  end 
+  pigeon_list
 end
